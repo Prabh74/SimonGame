daa320e517417df7e844d7db8b867e214045f532
Prabh74 <81363833+Prabh74@users.noreply.github.com>
2021-08-03 22:27:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
+// global variables
 var level = 0;
 var randomSeries = [];
 var userSeries = [];
+var gameState = false;
 
+
+// main
+$(document).keypress(function(){
+    if (!gameState){
+            $("h1").text("Level "+level);
+            sequence();
+            gameState=true;
+    }
+    
+})
+
+$("button").click(function (){
+     var userPressedButtonId ="#" + this.id;
+    $(userPressedButtonId).fadeOut(50).fadeIn(50);
+    userSeries.push(userPressedButtonId);
+    check(userSeries.length - 1);
+})
+
+
+
+// functions
 function sequence(){
     level++;
     userSeries = [];
     $("h1").text("Level " + level);
     var randomButtonId = "#btn" + (Math.floor(Math.random()*4) + 1);
     randomSeries.push(randomButtonId);
-    $(randomButtonId).fadeOut(50).fadeIn(50);
+    $(randomButtonId).fadeOut(100).fadeIn(100);
 }
 
-$(document).keypress(function(){
-    if (level === 0)
-    {
-        sequence();
-    }
-})
+function startOver(){
+    level=0;
+    randomSeries = [];
+    gameState = false;
+}
 
-    $("button").click(function (){
-        var userPressedButtonId ="#" + this.id;
-        $(userPressedButtonId).fadeOut(50).fadeIn(50);
-        userSeries.push(userPressedButtonId);
-        check();
-        if(userSeries.length === randomSeries.length){
+function check(indexTOCheck){
+    if(userSeries[indexTOCheck]===randomSeries[indexTOCheck])
+    {
+        if(userSeries.length===randomSeries.length){
             setTimeout(sequence,1000);
         }
-    })
-
-
-function check(){
-    for(var i=0; i<userSeries.length;i++)
-    {
-        if(userSeries[i] !== randomSeries[i])
-        {
-            $("h1").text("GAME OVER");
-            setTimeout(function(){
-                level = 0;
-                $("h1").text("Press any key to start");
-                randomSeries = [];
-                userSeries = [];
-            }, 2000)
-            
-        }
+    }
+    else{
+        $("h1").text("GAME OVER, press key to start again");
+        startOver();
     }
 }
+
+
+
+
